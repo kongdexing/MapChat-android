d45ca7c13e628209ce93b45ee55b575343ee95dc
kongdexing <171024402@qq.com>
2015-09-02 23:10:04 +0800

    prevent repeat toast

--- mapchat/src/main/java/cn/gdeveloper/mapchat/ui/WinToast.java
+++ mapchat/src/main/java/cn/gdeveloper/mapchat/ui/WinToast.java
@@ -7,52 +7,54 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import cn.gdeveloper.mapchat.R;
 
 public class WinToast {
 
-	public static void toast(Context context, int textRes) {
-		CharSequence text = context.getResources().getText(textRes);
-		makeText(context, text).show();
-	}
+    private static Toast toast;
+    private static Toast toastWithIcon;
 
-	public static void toast(Context context, CharSequence sequence) {
-		makeText(context, sequence).show();
-	}
+    public static void toast(Context context, int textRes) {
+        CharSequence text = context.getResources().getText(textRes);
+        makeText(context, text);
+    }
 
-	public static void toastWithCat(Context context, int textRes, boolean isHappy) {
-		CharSequence text = context.getResources().getText(textRes);
-		toastWithCat(context, text, isHappy);
-	}
+    public static void toast(Context context, CharSequence sequence) {
+        makeText(context, sequence);
+    }
 
-	public static void toastWithCat(Context context, CharSequence text, boolean isHappy) {
-		Toast result = new Toast(context);
+    public static void toastWithCat(Context context, int textRes, boolean isHappy) {
+        CharSequence text = context.getResources().getText(textRes);
+        toastWithCat(context, text, isHappy);
+    }
 
-		LayoutInflater inflate = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View v = inflate.inflate(R.layout.de_ui_toast, null);
-		result.setView(v);
-		ImageView iv = (ImageView) v.findViewById(android.R.id.icon);
+    public static void toastWithCat(Context context, CharSequence text, boolean isHappy) {
+        if (toastWithIcon == null) {
+            toastWithIcon = new Toast(context);
+        }
+        LayoutInflater inflate = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflate.inflate(R.layout.de_ui_toast, null);
+        toastWithIcon.setView(v);
+        ImageView iv = (ImageView) v.findViewById(android.R.id.icon);
+        TextView tv = (TextView) v.findViewById(android.R.id.message);
+        tv.setText(text);
+        toastWithIcon.setGravity(Gravity.CENTER, 0, 0);
+        toastWithIcon.setDuration(Toast.LENGTH_SHORT);
+        toastWithIcon.show();
+    }
 
-		TextView tv = (TextView) v.findViewById(android.R.id.message);
-		tv.setText(text);
-
-		result.setGravity(Gravity.CENTER, 0, 0);
-		result.setDuration(Toast.LENGTH_SHORT);
-		result.show();
-	}
-
-	public static Toast makeText(Context context, CharSequence text) {
-		Toast result = new Toast(context);
-
-		LayoutInflater inflate = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View v = inflate.inflate(R.layout.de_ui_toast, null);
-		result.setView(v);
-		TextView tv = (TextView) v.findViewById(R.id.message);
-		tv.setText(text);
-
-		result.setGravity(Gravity.CENTER, 0, 0);
-		result.setDuration(Toast.LENGTH_SHORT);
-
-		return result;
-	}
+    public static void makeText(Context context, CharSequence text) {
+        if (toast == null) {
+            toast = new Toast(context);
+        }
+        LayoutInflater inflate = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflate.inflate(R.layout.de_ui_toast, null);
+        toast.setView(v);
+        TextView tv = (TextView) v.findViewById(R.id.message);
+        tv.setText(text);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
